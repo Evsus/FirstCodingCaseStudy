c47e1cc0adb8ce3316c5e9825ba614a5bf90733d
Jereomy Ayres <ayresj@dewv.edu>
2017-11-09 02:12:02 +0000

    Elevator project#37

---
+++ projects/4ADTs/elevator/elevator.pl
@@ -0,0 +1,68 @@
+use strict;
+use warnings;
+use diagnostics;
+#salutation
+print "Welcome to Jereomy's elevator simulator!\n";
+
+#create subs
+sub GOTO_FLOOR{
+    (my $current_number , my $target_number) = @_;
+    for(my $i = (100 - abs($target_number - $current_number)) ; $i != 100 ; $i++){
+        if($current_number < $target_number){
+            $current_number++;
+            print "$current_number..";
+            }
+        elsif($current_number > $target_number){
+            $current_number--;
+            print "$current_number..";
+        }
+    }
+    print "Ding!\n";
+    return $current_number;
+    } #returns current floor after moving to target floor syntax: (start , finish)
+
+sub FIRE_ALARM{
+    my $ground_floor = 1;
+    my $current_floor = shift;
+    print "Danger!  You must exit the building now!\n";
+    unless($current_floor == $ground_floor){
+        GOTO_FLOOR($current_floor , $ground_floor);
+        }
+    return $ground_floor;
+    }
+
+#main script
+my $ground_floor = 1;
+my $current_floor = $ground_floor;
+my $top_floor = 100;
+while(){
+    print "Options: (s)elect a floor, (f)ire alarm, or (q)uit\n",
+    "Enter s, f, or q ==> ";
+    my $user_option = <>;
+    if($user_option =~ /q/i){
+        last;
+        }
+    elsif($user_option =~ /s/i){{
+        print "Enter the floor that you'd like to go to ==> ";
+        my $floor_input = <>;
+        if(($floor_input !~ /\d/) || ($floor_input > 100) || ($floor_input == 13)){
+            print "Invalid floor selection - must be between 1 and 100, excluding 13.\n";
+            last;
+            }
+        elsif($current_floor > $floor_input){
+            print "Going down!..";
+            }
+        elsif($current_floor < $floor_input){
+            print "Going up!..";
+            }
+        #else already there.
+        $current_floor = GOTO_FLOOR($current_floor , $floor_input);
+        }}
+    elsif($user_option =~ /f/i){
+        $current_floor = FIRE_ALARM($current_floor);
+        }
+    else{
+        print "Invalid selection.\n";
+        }
+    }
+#eof
